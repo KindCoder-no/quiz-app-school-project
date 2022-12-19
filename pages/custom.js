691a9b74557de3d361d74fe526d278edc51d2ab1
Emre Sanden <emresanden@gmail.com>
2022-12-19 10:48:37 +0100

    Improve UI

--- pages/custom.js
+++ pages/custom.js
@@ -57,9 +57,7 @@ export default function Custom() {
           <div className="col-md-6">
             <div
               style={{
-                background: "rgb(0,212,255)",
-                background:
-                  "linear-gradient(90deg, rgba(0,212,255,1) 0%, rgba(15,15,168,1) 50%, rgba(11,3,181,1) 100%)",
+                backgroundColor: "#0b5ed7",
                 borderRadius: 10
               }}
             >
